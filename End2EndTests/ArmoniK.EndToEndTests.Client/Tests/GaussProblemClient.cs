69cf4b090217262ad3c3280ff3ab3e87ebe2defe
PierrePicard20 <pierre.picard20@gmail.com>
2026-01-29 17:32:01 +0100

    fix: shared BlobDefinition instances should not lead to blob duplicates

--- End2EndTests/ArmoniK.EndToEndTests.Client/Tests/GaussProblemClient.cs
+++ End2EndTests/ArmoniK.EndToEndTests.Client/Tests/GaussProblemClient.cs
@@ -38,9 +38,10 @@ public class GaussProblemClient : ClientBase
   ///   Adds all integers from 1 to 10 (added two by two on worker side with subtasking)
   ///   and check that result is 55
   /// </summary>
-  [TestCase(10)]
-  public async Task GaussProblem(int N)
+  [Test]
+  public async Task GaussProblem()
   {
+    var N        = 10;
     var callback = new Callback();
     var task = new TaskDefinition().WithLibrary(WorkerLibrary!)
                                    .WithTaskOptions(TaskConfiguration!)
